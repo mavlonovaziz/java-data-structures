67569de19d28481424045c0cdcd932b65531e5cb
movlo <mo>
2022-11-08 14:32:35 +0500

    hammasi yaxshi hammasi yaxshi

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        List<Car.Cars> list = new ArrayList<>();
+        List<Cars> list = new ArrayList<>();
 
         Cars cars = new Cars("blur", "BMW", 253);
         Cars cars1 = new Cars("green", "Mercedes benz", 777);
-        Cars cars2 =new Cars("red","nexia",111);
-        Cars cars3 = new Cars("blue","onix",188);
-        Cars cars4 =new Cars("black","Audi",444);
-        Cars cars5 =new Cars("red","ferrari",111);
-        Cars cars6 =new Cars("blur","nexia3",696);
-        Cars cars7 =new Cars("black","cobalt",252);
-        Cars cars8 =new Cars("red","ford",969);
-        Cars cars9 =new Cars("blue","malibu",555);
+        Cars cars2 = new Cars("red", "nexia", 111);
+        Cars cars3 = new Cars("blue", "onix", 188);
+        Cars cars4 = new Cars("black", "Audi", 444);
+        Cars cars5 = new Cars("red", "ferrari", 111);
+        Cars cars6 = new Cars("blur", "nexia3", 696);
+        Cars cars7 = new Cars("black", "cobalt", 252);
+        Cars cars8 = new Cars("red", "ford", 969);
+        Cars cars9 = new Cars("blue", "malibu", 555);
 
-
-        //List<Cars> list = Arrays.asList(cars,cars1,cars2,cars3,cars4,cars5,cars6,cars7,cars8,cars9);
+        List<Cars> list1 = new ArrayList<>(5);
+        Cars cars10 = new Cars("blue", "bmw2", 456);
+        Cars cars11 = new Cars("red", "onix", 5858);
+        Cars cars12 = new Cars("black", "royin", 858);//List<Cars> list = Arrays.asList(cars,cars1,cars2,cars3,cars4,cars5,cars6,cars7,cars8,cars9);
+        Cars cars13 = new Cars("orange", "aff", 741);//List<Cars> list = Arrays.asList(cars,cars1,cars2,cars3,cars4,cars5,cars6,cars7,cars8,cars9);
+        Cars cars14 = new Cars("dracula", "asd", 852);//List<Cars> list = Arrays.asList(cars,cars1,cars2,cars3,cars4,cars5,cars6,cars7,cars8,cars9);
+        Cars cars15 = new Cars("dracon", "fgh", 963);//List<Cars> list = Arrays.asList(cars,cars1,cars2,cars3,cars4,cars5,cars6,cars7,cars8,cars9);
 
         list.add(cars1);
         list.add(cars);
@@ -32,6 +37,26 @@ public class Main {
         list.add(cars8);
         list.add(cars9);
 
+        list1.add(cars10);
+        list1.add(cars11);
+        list1.add(cars12);
+        list1.add(cars13);
+        list1.add(cars14);
+        list1.add(cars15);
+
+        list.addAll(0, list1);
+        list1.addAll(1, list);
+
+//        list.retainAll(list1);
+//
+//        list.removeAll(list1);
+//
+//
+//        System.out.println(list + "  ?");
+//
+//        System.out.println(list + "  /");
+
+        System.out.println(list1 + " |");
 
 
 //        System.out.println(Arrays.toString(new List[]{list}) + "  ");
@@ -41,10 +66,11 @@ public class Main {
         System.out.println(obj.getRandomElement(list));
     }
 
-    public Cars getRandomElement(List<Cars> list)
-    {
+    public Cars getRandomElement(List<Cars> list) {
         Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
+
+
 }
 
