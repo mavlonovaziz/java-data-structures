456173c09542bcc9ef0612cd15f20d0cd6c205dc
movlo <mo>
2022-11-10 18:09:26 +0500

    hammasi yaxshi dfddf

--- src/Sort.java
+++ src/Sort.java
@@ -1,16 +1,12 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Sort {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(List.of("bwvsdf","adsfsd"));
-
-        Stream<String>stringStream;
-        stringStream = list.stream().sorted();
-        list.forEach(System.out::println);
-
-            stringStream.forEach(System.out::println);
+        Set<User> userSet = new TreeSet<>(List.of(new User(22, "Tramp"), new User(25, "FT9")));
+        List<User> userList =userSet.stream().distinct().collect(Collectors.toList());
+        Stream<User> userStream = userSet.stream().sorted(Comparator.comparing(User::getShortName));
+        userStream.forEach(user -> System.out.println(user.toString()));
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,3 +1,33 @@
 public class User {
     private int age;
+    private String shortName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", shortName='" + shortName + '\'' +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public User(int age, String shortName) {
+        this.age = age;
+        this.shortName = shortName;
+    }
 }
